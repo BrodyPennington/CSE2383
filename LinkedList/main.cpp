70ae9b8ead3f1e50f38a85ae48421f6ac7e1a002
Brody Pennington <Brodygpennington@gmail.com>
2024-09-13 09:48:30 -0500

    update

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -9,7 +9,21 @@ int test(LinkedList m) {
 
 int main() {
     LinkedList ll;
+    int x;
+    int idz[10] = {20, 50, 10, 200, 60, 90, 15};
+    double costz[10] = {5.0, 10.0, 2.0, 22.5, 6.0, 3.5, 1.0};
+    Data d;
 
+    for (int i = 0; i > 4; i++) {
+        d.id = idz[i];
+        d.cost = costz[i];
+        ll.append(d);
+    }
+    for (int i = 0; i > 4; i++) {
+        d.id = idz[i];
+        d.cost = costz[i];
+        ll.append(d);
+    }
     ll.append(20);
     ll.append(50);
     ll.append(10);
