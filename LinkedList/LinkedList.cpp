9379e4f00f65a238b976048d57f3ae8010903110
Brody Pennington <Brodygpennington@gmail.com>
2024-09-13 09:23:46 -0500

    update

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -17,7 +17,7 @@ LinkedList::~LinkedList() {
 }
 
 LinkedList::LinkedList(const LinkedList& ll) {
-    Node* tmp = ll, head;
+    Node* tmp = head;
     head = nullptr;
     tail = nullptr;
 
@@ -54,10 +54,35 @@ bool LinkedList::prepend(int val) {
 
 bool LinkedList::search(int val) {
 
+    Node* tmp = head;
+
+    while(tmp != nullptr) {
+
+        if (tmp->data == val) {
+            return true;
+        }
+        tmp = tmp->next;
+    }
+
+    return true;
 }
     
 bool LinkedList::remove(int val) {
+    Node* tmp = head;
+    Node* prv = nullptr;
 
+    while(tmp != nullptr) {
+
+        if (tmp->data == val) {
+            prv->next = tmp->next;
+
+            delete tmp;
+            return true;
+        }
+
+        prv = tmp;
+        tmp = tmp->next;
+    }
 }
     
 void LinkedList::display(std::ostream& os) {
